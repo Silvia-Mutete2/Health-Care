51df9fe146313e5442ef972ece3118a8c6e04ebe
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-06-22 21:47:40 +0300

    scroll

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    //For the navbar to appear on scroll
+    
+    $(window).scroll(function(){
+        if(this.scrollY >200){
+            $('.navbar').addClass('sticky')
+        }else{
+            $('.navbar').removeClass('sticky')
+            }
+        })
+    })
